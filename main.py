e53a73044ff1daab20eb359ba718522191f21168
fuste-r <cjbookheimer@gmail.com>
2026-02-16 16:28:05 -0500

    add argsparse for user prompt

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import argparse
 from dotenv import load_dotenv
 from google import genai
 
@@ -7,16 +8,21 @@ load_dotenv()
 api_key= os.environ.get("GEMINI_API_KEY")
 client = genai.Client(api_key=api_key)
 
+
+
 def main():
-    prompt="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+    #prompt="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+    parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    args = parser.parse_args()
     
-    response = client.models.generate_content(model = 'gemini-2.5-flash',contents=prompt )
+    response = client.models.generate_content(model = 'gemini-2.5-flash',contents=args.user_prompt)
 
     if response.usage_metadata.prompt_token_count == None:
         raise RuntimeError("Failed API request")
 
 
-    print(f"User prompt: {prompt}")
+    print(f"User prompt: {args.user_prompt}")
     print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
     print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
     print("Response:")
