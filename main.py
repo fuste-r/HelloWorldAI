9e313551ad54a8e2907e0d08914c5ae96907ce0d
fuste-r <cjbookheimer@gmail.com>
2026-02-16 15:47:42 -0500

    added prompt and response token count

--- main.py
+++ main.py
@@ -8,10 +8,18 @@ api_key= os.environ.get("GEMINI_API_KEY")
 client = genai.Client(api_key=api_key)
 
 def main():
-    print("Hello from helloworldai!")
-    contents = ()
+    prompt="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
     
-    response = client.models.generate_content(model = 'gemini-2.5-flash',contents= "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
+    response = client.models.generate_content(model = 'gemini-2.5-flash',contents=prompt )
+
+    if response.usage_metadata.prompt_token_count == None:
+        raise RuntimeError("Failed API request")
+
+
+    print(f"User prompt: {prompt}")
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    print("Response:")
     print(response.text)
 
 if __name__ == "__main__":
